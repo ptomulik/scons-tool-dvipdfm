28a8701516e2f5302a7983fdf7f5600b6f0be78e
Paweł Tomulik <ptomulik@meil.pw.edu.pl>
2020-04-17 08:40:44 +0200

    Fixed issue #2

--- test/system/sconstool/dvipdfm/dvipdfm_with_wrapper.py
+++ test/system/sconstool/dvipdfm/dvipdfm_with_wrapper.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (c) 2013-2018 by Paweł Tomulik <ptomulik@meil.pw.edu.pl>
+# Copyright (c) 2013-2020 by Paweł Tomulik <ptomulik@meil.pw.edu.pl>
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
